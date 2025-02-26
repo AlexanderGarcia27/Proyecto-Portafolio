4f5c74ee67597c3ab3ba4abca1d2310dfed385b9
Alexander García <agr270404@gmail.com>
2025-02-26 16:26:38 -0600

    [ADD] Se agrego section de presentacion

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
-// script.js
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
     const menuToggle = document.getElementById("menuToggle");
     const menuList = document.getElementById("menuList");
 
-    menuToggle.addEventListener("click", function () {
+    // Alternar la visibilidad del menú al hacer clic en el botón
+    menuToggle.addEventListener("click", (event) => {
         menuList.classList.toggle("header__list--active");
+        event.stopPropagation(); // Evita que el clic en el botón cierre el menú
+    });
+
+    // Cerrar el menú si se hace clic fuera de él
+    document.addEventListener("click", (event) => {
+        if (!menuList.contains(event.target) && !menuToggle.contains(event.target)) {
+            menuList.classList.remove("header__list--active");
+        }
     });
 });
+
